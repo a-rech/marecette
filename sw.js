087fd7f79af3cbe6e53d3ec693d4500d3561bc01
a-rech <alexrech@live.fr>
2026-09-26 15:45:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ════════════════════════════════════════════════════════════════════════
-   MaRecette — Service Worker v1.4.3
+   MaRecette — Service Worker v1.4.4
    ────────────────────────────────────────────────────────────────────────
    Stratégie : Cache First + notification de mise à jour.
 
